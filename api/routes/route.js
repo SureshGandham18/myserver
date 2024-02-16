5bdf8db882534eadc2900cb9c617a30653f01ae6
SureshGandham18 <20pa1a0540@vishnu.edu.in>
2024-02-16 14:59:50 +0530

    modified

--- api/routes/route.js
+++ api/routes/route.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { info} from '../controllers/controller.js';
 
 const router = express.Router();
-router.get("/info", info);
-// router.post("/info",info)
+// router.get("/info", info);
+router.post("/info",info)
 export default router;
